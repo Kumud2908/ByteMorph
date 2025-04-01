64af14f68f1e1e8c4aed9c2a18330b53ad01496c
Kumud2908 <sagarkumud561@gmail.com>
2025-04-02 01:52:49 +0530

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Signin from './pages/Signin'
+import About from './pages/About'
 
 export default function App() {
   return (
     <div>
+     <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+      
+    </BrowserRouter>
       
     </div>
   )
