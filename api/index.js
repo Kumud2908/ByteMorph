12c901c52e64ddb07387fe706db7b8554cca539b
Kumud2908 <sagarkumud561@gmail.com>
2025-04-02 11:07:09 +0530

    created signin api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 const app=express();
+app.use(express.json());
 mongoose
  .connect(process.env.MONGO_URI)
  .then(()=>{
@@ -13,4 +16,7 @@ mongoose
  });
 app.listen(3000 ,()=>{
     console.log('server lsitening on port 3000!')
-})
+});
+
+app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
