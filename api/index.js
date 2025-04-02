3560844c912eb94bfce3f25784c587a8abf54faf
Kumud2908 <sagarkumud561@gmail.com>
2025-04-02 09:39:30 +0530

    made a connection to mongoose and resolve the time non-syncing error from my local pc

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import express from'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
+mongoose
+ .connect(process.env.MONGO_URI)
+ .then(()=>{
+    console.log('Connected to Mongoose');
+ })
+ .catch((err)=>{
+    console.log(err);
+ });
 app.listen(3000 ,()=>{
     console.log('server lsitening on port 3000!')
 })
